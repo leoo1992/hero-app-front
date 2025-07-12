3fe674a8ba014c5536124c1321d6dff4d82dd056
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2025-07-11 23:44:56 -0300

    add 404page

---
+++ src/pages/PaginaNaoEncontrada.tsx
@@ -0,0 +1,10 @@
+export default function PaginaNaoEncontrada() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold text-blue-600">404</h1>
+        <p className="text-xl mt-2 text-gray-700">Página não encontrada</p>
+      </div>
+    </div>
+  );
+}
