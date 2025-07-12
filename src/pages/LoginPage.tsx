f362cc97069496527bab5326d4984a4ab399c2d5
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2025-07-11 23:47:25 -0300

    adjust pages

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -7,12 +7,14 @@ export default function LoginPage() {
     <section className="flex items-center justify-center min-h-full p-4">
       <form className="bg-white p-8 rounded shadow-md w-full max-w-md space-y-4">
         <h2 className="text-2xl font-bold text-center">Login do Herói</h2>
-
-        <Input type="email" placeholder="Email" required />
+        <Input
+          type="email"
+          placeholder="Email"
+          required
+          autoComplete="username"
+        />
         <Input type="password" placeholder="Senha" required />
-
         <Button type="submit">Entrar</Button>
-
         <p className="text-center text-sm text-gray-600">
           Ainda não tem conta?{" "}
           <Link to="/register" className="text-blue-500 hover:underline">
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -4,11 +4,24 @@ import Button from "../components/ui/Button";
 export default function RegisterPage() {
   return (
     <section className="flex items-center justify-center min-h-full p-4">
-      <form className="bg-white p-8 rounded shadow-md w-full max-w-md space-y-4">
+      <form
+        className="bg-white p-8 rounded shadow-md w-full max-w-md space-y-4"
+        autoComplete="off"
+      >
         <h2 className="text-2xl font-bold text-center">Cadastro de Herói</h2>
-        <Input type="text" placeholder="Nome do Herói" required />
-        <Input type="email" placeholder="Email" required />
-        <Input type="password" placeholder="Senha" required />
+        <Input
+          type="text"
+          placeholder="Nome do Herói"
+          required
+          autoComplete="off"
+        />
+        <Input type="email" placeholder="Email" required autoComplete="off" />
+        <Input
+          type="password"
+          placeholder="Senha"
+          required
+          autoComplete="off"
+        />
         <Button type="submit">Registrar</Button>
       </form>
     </section>
