3ffa834754b64ee7f4a74ca8fc6eb15fee8a1abd
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2025-07-13 20:05:05 -0300

    add lists and adjust responsivity

--- src/components/layouts/Drawer.tsx
+++ src/components/layouts/Drawer.tsx
@@ -1,7 +1,8 @@
 import { GiBatMask } from "react-icons/gi";
+import { FaTimes } from "react-icons/fa";
 export default function Drawer() {
   return (
-    <div className="max-w-screen">
+    <div className="max-w-full">
       <input id="my-drawer" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex items-center">
         <label
@@ -24,38 +25,58 @@ export default function Drawer() {
         <span className="ml-6 text-xl text-indigo-900 ">Hero Force</span>
       </div>
 
-      <div className="drawer-side w-screen max-w-screen">
+      <div className="drawer-side w-screen h-screen">
         <label
           htmlFor="my-drawer"
           aria-label="close sidebar"
-          className="bg-gradient-to-br from-purple-900 to-emerald-900 opacity-40 h-screen max-screen w-screen max-w-screen"
+          className="bg-gradient-to-br from-purple-900 to-emerald-900 opacity-40 w-screen h-screen"
         ></label>
-        <div className="max-w-56 bg-gradient-to-br from-emerald-300 to-purple-300 border-r-3 border-r-purple-300 text-base-content min-h-full w-80 p-4">
+        <div className="sm:max-w-56 bg-gradient-to-br from-emerald-300 to-purple-300 border-r-3 border-r-purple-300 text-base-content min-h-full w-80 p-4">
+          <div className="flex text-end justify-end w-full mb-3 pr-3 text-indigo-800">
+            <label
+              htmlFor="my-drawer"
+              className="btn close-btn btn-square rotate-10 btn-primary"
+            >
+              <FaTimes className="text-lg" />
+            </label>
+          </div>
           <div className="bg-base-100 border-base-300 collapse border">
             <input type="checkbox" className="peer" />
-            <div className="collapse-title bg-primary text-primary-content peer-checked:bg-purple-500 peer-checked:text-secondary-content">
+            <div className="collapse-title bg-accent-content text-primary-content peer-checked:bg-purple-500 peer-checked:text-secondary-content">
               Perfil
             </div>
-            <div className="collapse-content bg-primary text-primary-content peer-checked:bg-purple-500 peer-checked:text-secondary-content">
-              rota 1
+            <div className="collapse-content text-primary-content peer-checked:bg-purple-500 peer-checked:text-secondary-content">
+              <ul className="space-y-1">
+                <li>Selecionar Herói</li>
+                <li>Editar Herói</li>
+                <li>Excluir Herói</li>
+              </ul>
             </div>
           </div>
           <div className="bg-base-100 border-base-300 collapse border">
             <input type="checkbox" className="peer" />
-            <div className="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+            <div className="collapse-title bg-indigo-950 text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
               Ranking
             </div>
-            <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
-              rota 1
+            <div className="collapse-content text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+              <ul className="space-y-1">
+                <li>Por Heróis</li>
+                <li>Por Status</li>
+              </ul>
             </div>
           </div>
           <div className="bg-base-100 border-base-300 collapse border">
             <input type="checkbox" className="peer" />
-            <div className="collapse-title bg-primary text-primary-content peer-checked:bg-teal-600 peer-checked:text-secondary-content">
+            <div className="collapse-title bg-purple-950 text-primary-content peer-checked:bg-teal-600 peer-checked:text-secondary-content">
               Missões
             </div>
-            <div className="collapse-content bg-primary text-primary-content peer-checked:bg-teal-600 peer-checked:text-secondary-content">
-              rota 1
+            <div className="collapse-content text-primary-content peer-checked:bg-teal-600 peer-checked:text-secondary-content">
+              <ul className="space-y-1">
+                <li>Iniciar Missão</li>
+                <li>Cadastrar Missão</li>
+                <li>Listar Missões</li>
+                <li>Excluir Missão</li>
+              </ul>
             </div>
           </div>
         </div>
