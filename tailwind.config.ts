24fc091de204fdf73a793117519029a856ecaa4a
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2025-07-13 03:19:54 -0300

    configs

---
+++ tailwind.config.ts
@@ -0,0 +1,39 @@
+import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
+
+// @ts-expect-error: DaisyUI não tem tipos para temas
+import themes from "daisyui/src/colors/themes";
+
+const config: Config = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+};
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+config.daisyui = {
+  themes: [
+    {
+      light: {
+        ...themes["[data-theme=light]"],
+      },
+      dark: {
+        ...themes["[data-theme=dark]"],
+      },
+      aqua: {
+        ...themes["[data-theme=aqua]"],
+      },
+      forest: {
+        ...themes["[data-theme=forest]"],
+      },
+      corporate: {
+        ...themes["[data-theme=corporate]"],
+      },
+    },
+  ],
+};
+
+export default config;
