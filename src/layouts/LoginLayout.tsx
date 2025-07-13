30ee840a844e78a98d00370b8bfc17063e288095
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2025-07-13 13:33:15 -0300

    adjust login layout

--- src/layouts/LoginLayout.tsx
+++ src/layouts/LoginLayout.tsx
@@ -7,14 +7,18 @@ export default function LoginLayout({
   readonly children: ReactNode;
 }) {
   return (
-    <main className="relative h-screen w-full flex items-end justify-center overflow-hidden">
+    <div className="w-full h-full flex flex-col">
       <img
         src={batman}
         alt="Batman"
-        className="absolute top-0 left-0 inset-0 w-full h-full object-cover opacity-80"
+        className="absolute top-0 right-0 inset-0 w-full h-full object-cover opacity-80"
         style={{ zIndex: -1 }}
       />
-      <section className="flex sm:justify-start sm: z-10 w-full max-w-sm sm:p-6">{children}</section>
-    </main>
+      <main className="h-screen flex flex-col justify-end items-start overflow-hidden">
+        <section className="z-10 m-2 sm:ml-16 sm:p-6">
+          {children}
+        </section>
+      </main>
+    </div>
   );
 }
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,5 @@
 import { useState, type FormEvent, useContext } from "react";
+import { GiBatMask } from "react-icons/gi";
 import { useNavigate, Link } from "react-router-dom";
 import AuthContext from "@/contexts/authContext";
 import InputMail from "../components/ui/InputMail";
@@ -30,11 +31,17 @@ export default function LoginPage() {
   }
 
   return (
-    <section className="hero min-w-full min-h-full text-indigo-800 font-semibold p-0 m-0 flex lg:justify-self-start lg:justify-start lg:content-start">
-      <div className="hero-content flex-col lg:flex-row-reverse gap-2 w-full p-0 m-0 flex lg:justify-items-start" >
-        <div className="opacity-90 text-center lg:text-left bg-emerald-50 p-2 sm:p-4 rounded-xl md:rounded-3xl">
-          <h1 className="text-2xl text-center font-bold w-72">Login do Heroi !</h1>
-          <p className="pt-6 max-w-72 leading-relaxed hidden lg:block">
+    <section className="hero min-h-full w-full text-indigo-800 font-semibold p-0 m-0 flex flex-col">
+      <div className="hero-content flex-col lg:flex-row-reverse gap-2 sm:gap-8 w-full p-0 m-0 flex lg:justify-content-end">
+        <div className="opacity-90 text-center lg:text-left bg-emerald-50 p-2 sm:p-6 rounded-xl md:rounded-3xl">
+          <div className="w-0 h-0 justify-items-center text-6xl hidden md:block sm:-rotate-20">
+              <GiBatMask />
+          </div>
+
+          <h1 className="text-2xl text-center font-bold w-72">
+            Login do Herói !
+          </h1>
+          <p className="pt-6 max-w-72 md:leading-relaxed hidden md:block">
             Seja bem-vindo(a) ao HeroForce, onde cada clique é um passo na sua
             jornada de transformação. Faça login e continue sendo o herói da sua
             história.
@@ -42,7 +49,7 @@ export default function LoginPage() {
         </div>
         <div className="card p-3 sm:p-4 mb-6 sm:0 rounded-3xl w-full max-w-sm shrink-0 shadow-xl bg-base-100 opacity-90">
           <div className="card-body">
-            <form className="fieldset" onSubmit={handleSubmit}>
+            <form className="fieldset space-y-2" onSubmit={handleSubmit}>
               <InputMail
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
@@ -52,8 +59,13 @@ export default function LoginPage() {
                 onChange={(e) => setSenha(e.target.value)}
               />
               <div className="flex gap-2 w-full pt-6 justify-end sm:justify-center">
-                <span className="text-indigo-500 hidden sm:block"> Ainda não tem conta?</span>
-                <Link to="/register" className="text-blue-500 hover:underline text-center">
+                <span className="text-indigo-500 hidden sm:block">
+                  Ainda não tem conta?
+                </span>
+                <Link
+                  to="/register"
+                  className="text-blue-500 hover:underline text-center"
+                >
                   Cadastre-se
                 </Link>
               </div>
